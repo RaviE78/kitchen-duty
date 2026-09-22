f71bba600c4a735d44a9633cc819b0d946e95209
RaviE78 <raviverma11072003@gmail.com>
2026-09-22 16:15:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "YOUR_SUPABASE_URL";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+window.SUPABASE_URL = "https://pafkmdecwsijvzbzvndt.supabase.co/rest/v1/";
+window.SUPABASE_ANON_KEY = "sb_publishable_U0r9gj5rZ1dJe2-Rncv-3w_n-8EYlh-";
